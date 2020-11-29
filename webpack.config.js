f3ef3dd4771d6fa4c09c0062419a5cb32b78e7b1
LuxAeterna-git <68290402+LuxAeterna-git@users.noreply.github.com>
2020-11-29 18:07:31 +0200

    Сделал первую сборку вебпака
    Пока есть ошибка в компиляции стилей вместе с паг

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+ const path = require('path')
+ const HTMLWebpackPlagin = require('html-webpack-plugin')
+ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+ module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlagin({
+            filename: `index.html`,
+            template: './src/index.pug',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: 'pug-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin, 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [ {
+                    loader: MiniCssExtractPlugin.loader
+                  },
+                  'css-loader', 'sass-loader'
+                ]
+            }
+        ]
+    },
+    
+}
